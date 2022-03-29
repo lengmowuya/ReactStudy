75b737a0a5fa4a4b068927c0502d9aecb58547f5
lengmowuya <3537168410@qq.com>
2022-03-30 00:11:29 +0800

    react代理配置学习

--- 4.react-ajax/src/App.jsx
+++ 4.react-ajax/src/App.jsx
@@ -3,7 +3,13 @@ import axios from 'axios'
 
 export default class App extends Component {
   getStudentData = ()=>{
-    axios.get('/getStudent').then(
+    axios.get('/api1/students').then(
+      response => {console.log("成功数据请求！",response.data)},
+      error => {console.warn("数据请求失败！",error)}
+    )
+  }
+  getCarData = ()=>{
+    axios.get('/api2/cars').then(
       response => {console.log("成功数据请求！",response.data)},
       error => {console.warn("数据请求失败！",error)}
     )
@@ -12,6 +18,7 @@ export default class App extends Component {
     return (
       <div>
         <button onClick={this.getStudentData}>点击获取学生数据</button>
+        <button onClick={this.getCarData}>点击获取汽车数据</button>
       </div>
     )
   }
